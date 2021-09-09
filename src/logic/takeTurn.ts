1c15308f62c175b300680b4813dab8128c354556
Jonathan Giamporcaro <jonathan.giamporcaro@gmail.com>
2021-09-09 12:26:40 +0200

    add betAmount constant

--- src/logic/takeTurn.ts
+++ src/logic/takeTurn.ts
@@ -1,8 +1,10 @@
 import { wallet } from "../wallet";
 import { connect4Contract } from "../contract";
+import { ethers } from "ethers";
 
-/* TODO: Bet prop has to be a bignumber.
-Take a look to the ethers.js documentation to type it correctly. */
-export async function takeTurn(gameId: number, x: number, bet: any): Promise<void> {
+// use me
+const betAmount = ethers.utils.parseEther("0.001");
+
+export async function takeTurn(gameId: number, x: number): Promise<void> {
     throw new Error("implement me :)");
 }
