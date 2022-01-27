487dbf063c52cfa73b9ddbf3bf14437a4378d3a9
jgiamporcaro-ledger <jonathan.giamporcaro@gmail.com>
2022-01-27 10:37:46 +0100

    taketurn - remove bignumber helper

--- src/logic/takeTurn.ts
+++ src/logic/takeTurn.ts
@@ -2,9 +2,6 @@ import { wallet } from "../wallet";
 import { connect4Contract } from "../contract";
 import { ethers } from "ethers";
 
-// use me
-const betAmount = ethers.utils.parseEther("0.001");
-
 export async function takeTurn(gameId: number, x: number): Promise<void> {
     throw new Error("implement me :)");
 }
